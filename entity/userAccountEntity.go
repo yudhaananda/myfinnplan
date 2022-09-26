fcdbdc86c3cea1fe910a69794f573d9f67ebd6c6
Yudha Ananda <yudha.ananda.kresna@bni.co.id>
2022-09-26 15:48:26 +0700

    test useraccount

--- entity/userAccountEntity.go
+++ entity/userAccountEntity.go
@@ -3,13 +3,14 @@ package entity
 import "time"
 
 type UserAccount struct {
-	Id          int `gorm:"primarykey;autoIncrement:true"`
-	AccountName string
-	UserId      int
-	CreatedBy   string
-	CreatedDate time.Time
-	UpdatedBy   string
-	UpdatedDate time.Time
-	DeletedBy   string
-	DeletedDate time.Time
+	Id           int           `gorm:"primarykey;autoIncrement:true"`
+	BankAccounts []BankAccount `gorm:"ForeignKey:UserAccountId"`
+	AccountName  string
+	UserId       int
+	CreatedBy    string
+	CreatedDate  time.Time
+	UpdatedBy    string
+	UpdatedDate  time.Time
+	DeletedBy    string
+	DeletedDate  time.Time
 }
