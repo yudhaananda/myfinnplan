44a21234d7fb5eb5c87656bdb3a1640bb0d13ce9
Yudha Ananda <yudha.ananda.kresna@bni.co.id>
2022-10-12 16:12:19 +0700

    remove password from edit

--- input/userInput.go
+++ input/userInput.go
@@ -11,8 +11,6 @@ type UserInput struct {
 type UserEditInput struct {
 	Id        int    `json:"id" binding:"required"`
 	UserName  string `json:"username" binding:"required"`
-	Password  string `json:"password" binding:"required"`
-	Email     string `json:"email" binding:"required"`
 	Photo     string `json:"photo"`
 	Telephone string `json:"telephone"`
 }
