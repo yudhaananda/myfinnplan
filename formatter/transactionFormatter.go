10edbd17c7794af1339daeae3d910f02d18cb055
Yudha Ananda <yudha.ananda.kresna@bni.co.id>
2022-10-21 10:39:35 +0700

    normalize

--- formatter/transactionFormatter.go
+++ formatter/transactionFormatter.go
@@ -3,16 +3,17 @@ package formatter
 import (
 	"myfinnplan/entity"
 	"strconv"
+	"strings"
 	"time"
 )
 
 type TransactionFormatter struct {
 	Week                map[string][]entity.Transaction
 	WeekTotal           map[string]float64
-	WeekTotalNormalize  map[string]float64
+	WeekTotalNormalize  map[string]map[string]float64
 	Month               map[string][]entity.Transaction
 	MonthTotal          map[string]float64
-	MonthTotalNormalize map[string]float64
+	MonthTotalNormalize map[string]map[string]float64
 }
 
 func FormatTransaction(transaction []entity.Transaction) TransactionFormatter {
@@ -22,8 +23,8 @@ func FormatTransaction(transaction []entity.Transaction) TransactionFormatter {
 	result.Month = make(map[string][]entity.Transaction)
 	result.WeekTotal = make(map[string]float64)
 	result.MonthTotal = make(map[string]float64)
-	result.WeekTotalNormalize = make(map[string]float64)
-	result.MonthTotalNormalize = make(map[string]float64)
+	result.WeekTotalNormalize = make(map[string]map[string]float64)
+	result.MonthTotalNormalize = make(map[string]map[string]float64)
 
 	for _, value := range transaction {
 		year, week := value.CreatedDate.ISOWeek()
@@ -52,34 +53,63 @@ func FormatTransaction(transaction []entity.Transaction) TransactionFormatter {
 
 	}
 
-	// result.WeekTotalNormalize = normalizeWeek(result.WeekTotal)
-	// result.MonthTotalNormalize = normalizeMonth(result.MonthTotal)
+	result.WeekTotalNormalize = normalizeWeek(result.MonthTotal, result.WeekTotal)
+	result.MonthTotalNormalize = normalizeMonth(result.MonthTotal)
 
 	return result
 }
 
-// func normalizeWeek(weekTotal map[string]float64) (result map[string]float64) {
-// 	result = make(map[string]float64)
-// 	for _, val := range weekTotal {
+func normalizeMonth(monthTotal map[string]float64) (temp map[string]map[string]float64) {
+	temp = make(map[string]map[string]float64)
+	for key, v := range monthTotal {
+		year := strings.Split(key, " ")[1]
+		if temp[year] == nil {
+			temp[year] = make(map[string]float64)
+		}
+		temp[year][key] = v
+	}
+	for key, val := range temp {
+		_, max := findMinAndMax(val)
+		for idx, v := range val {
+			temp[key][idx] = ((v - 0) / (max - 0)) * 10
+		}
+	}
+	return temp
+}
 
-// 	}
-// 	return result
-// }
+func normalizeWeek(monthTotal map[string]float64, weekTotal map[string]float64) (temp map[string]map[string]float64) {
+	temp = make(map[string]map[string]float64)
+	for monthKey := range monthTotal {
+		temp[monthKey] = make(map[string]float64)
+		for weekKey, week := range weekTotal {
+			if strings.Contains(weekKey, monthKey) {
+				temp[monthKey][weekKey] = week
+			}
+		}
+	}
+	for key, val := range temp {
+		_, max := findMinAndMax(val)
+		for idx, v := range val {
+			temp[key][idx] = ((v - 0) / (max - 0)) * 10
+		}
+	}
+	return temp
+}
 
-// func findMinAndMax(a map[string]float64) (min float64, max float64) {
-// 	i := 0
-// 	for _, value := range a {
-// 		if i == 0 {
-// 			max = value
-// 			min = value
-// 		}
-// 		if value < min {
-// 			min = value
-// 		}
-// 		if value > max {
-// 			max = value
-// 		}
-// 		i++
-// 	}
-// 	return min, max
-// }
+func findMinAndMax(a map[string]float64) (min float64, max float64) {
+	i := 0
+	for _, value := range a {
+		if i == 0 {
+			max = value
+			min = value
+		}
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+		i++
+	}
+	return min, max
+}
